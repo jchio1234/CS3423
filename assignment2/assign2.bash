90bcc5611ad8fd8c2a60f0ad0aef96b496d011ab
rafrdz <rafrdz@RafaelCustom-2018.localdomain>
2018-09-27 20:09:37 -0500

    Finished assignment2

--- assignment2/assign2.bash
+++ assignment2/assign2.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#Name of the file is stored in $1
-
 #Remove trailing whitespace after a line
 sed -E -f trailingwhitespace.sed ${1} > debug.c
 
@@ -12,7 +10,7 @@ sed -E -f spacebetweenoperators.sed debug.c > debug2.c
 sed -E -f spacebetweentokens.sed debug2.c > debug3.c
 
 #Remove extra whitespace inside of parentheses
-sed -E -f spaceinconditions.sed debug3.c > testoutput.c
+sed -E -f spaceinconditions.sed debug3.c
 
 #Remove debug files
 rm debug.c
