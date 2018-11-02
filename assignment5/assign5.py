c22290bd5cc5f54f1c6e580bb25117f488c3a8f4
Rafael Rodriguez <rrodriguez1989@gmail.com>
2018-11-01 21:45:09 -0500

    Finished up main assignment, need to test extra credit

--- assignment5/assign5.py
+++ assignment5/assign5.py
@@ -37,12 +37,71 @@ def check_for_invalid_inventory(current_quant_string, max_quant_string):
     return False
 
 
-#Creates the '.output' file
-def create_output(file_name, s_name, i_name, c_quan, m_quan, body):
-    item = re.match(r'.*(?P<num>\d\d\d\d)\.item', file_name)
-    print('Item Number is: ' + item['num'])
-    print('Now we just need to sub and write to a ".out" file!')
+#Substitue simple_name variable
+def process_simple_name(s_name, string):
+    s_name_variable = left_char + 'simple_name' + right_char
+    processed_s_name = re.sub(s_name_variable, s_name, string)
+    return processed_s_name
 
+
+#Substitute item_name variable
+def process_item_name(i_name, string):
+    i_name_variable = left_char + 'item_name' + right_char
+    processed_i_name = re.sub(i_name_variable, i_name, string)
+    return processed_i_name
+
+
+#Substitute current_quantity variable
+def process_current_quantity(c_quan, string):
+    c_quan_variable = left_char + 'current_quantity' + right_char
+    processed_c_quan = re.sub(c_quan_variable, c_quan, string)
+    return processed_c_quan
+
+
+#Substitute max_quantity variable
+def process_max_quantity(m_quan, string):
+    m_quan_variable = left_char + 'max_quantity' + right_char
+    processed_m_quan = re.sub(m_quan_variable, m_quan, string)
+    return processed_m_quan
+
+#Substitute body variable
+def process_body(body, string):
+    body_variable = left_char + 'body' + right_char
+    processed_body = re.sub(body_variable, body, string)
+    return processed_body
+
+#Substitute date variable
+def process_date(string):
+    date_variable = left_char + 'date' + right_char
+    processed_date = re.sub(date_variable, g_date, string)
+    return processed_date
+
+
+#Create the output file
+def create_output_file(output_file, lines_to_write):
+    with open(output_file, 'a') as outfile:
+        for line in lines_to_write:
+            outfile.write(line)
+
+#Creates the '.output' file
+def process_lines(file_name, s_name, i_name, c_quan, m_quan, body):
+    item = re.match(r'.*(?P<num>\d\d\d\d)\.item', file_name)
+    output_file_name = item['num'] + '.item'
+    output_directory = output_dir + '/' + output_file_name
+    lines_to_write = []
+
+    #Open the template file and read in one line at a time
+    with open(template, 'r') as template_file:
+        for line in template_file:
+            simple_name = process_simple_name(s_name, line)
+            item_name = process_item_name(i_name, simple_name)
+            current_quantity = process_current_quantity(c_quan, item_name)
+            max_quantity = process_max_quantity(m_quan, current_quantity)
+            p_body = process_body(body, max_quantity)
+            p_date = process_date(p_body)
+            lines_to_write.append(p_date)
+
+    create_output_file(output_directory, lines_to_write)
 
 #Process files
 def process_files(file_list):
@@ -60,7 +119,7 @@ def process_files(file_list):
                     item = re.match(r'(?P<current_quant>\d+)\s+(?P<max_quant>\d+)', line)
                 if line_num == 3:
                     body = re.match(r'^[^\n]*$', line)
-        create_output(f.name, name['simple_name'], name['item_name'], item['current_quant'], item['max_quant'], body.group(0))
+        process_lines(f.name, name['simple_name'], name['item_name'], item['current_quant'], item['max_quant'], body.group(0))
 
 #Verify that the user entered the minimum number of arguments or exit with usage message
 if len(sys.argv) < 5:
@@ -74,8 +133,8 @@ if not os.path.exists(sys.argv[4]):
 
 #Check for extra credit args
 if len(sys.argv) > 5:
-    left_char = sys.argv[5]
-    right_char = sys.argv[6]
+    left_char = sys.argv[5].replace("'", "")
+    right_char = sys.argv[6].replace("'", "")
 else:
     left_char = '<<'
     right_char = '>>'
@@ -84,6 +143,7 @@ else:
 data_dir = sys.argv[1]
 template = sys.argv[2]
 g_date = sys.argv[3]
+output_dir = sys.argv[4]
 
 #Get all files in data directory
 all_files = list_item_files(data_dir)
