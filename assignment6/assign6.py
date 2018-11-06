ef997234e295b7b474ed25a529b1a3cf7c9d12a8
Rafael Rodriguez <rrodriguez1989@gmail.com>
2018-11-05 18:00:23 -0600

    Working on moving each file into the appropriate directory

--- assignment6/assign6.py
+++ assignment6/assign6.py
@@ -6,16 +6,25 @@ import re
 import shutil
 
 
-# Create directory if it doesn't exist
+# Create directory if it does not exist
 def check_if_directory_exists(directory_suffix):
     temp_dir_name = current_directory + '/assignment' + directory_suffix
     if not os.path.isdir(temp_dir_name):
         os.mkdir(temp_dir_name)
     return temp_dir_name
 
+
+# Create 'misc' directory if it does not exist
+def create_misc_directory():
+    temp_dir_name = current_directory + '/misc'
+    if not os.path.isdir(temp_dir_name):
+        os.mkdir(temp_dir_name)
+
+
 # Move the file to the specified directory
-def move_file(file, dir):
-    shutil.move(file, dir)
+def move_file(file_name, destination):
+    shutil.move(file, dest)
+
 
 # Verify the correct number of arguments
 if len(sys.argv) < 2:
@@ -34,14 +43,11 @@ current_directory = sys.argv[1]
 
 # Get all files in the directory
 all_files = os.listdir(current_directory)
-print('Number of files in ' + current_directory + ' is: ' + str(len(all_files)))
-
 
 # Iterate through each file in the directory
 for file in all_files:
     suffix = re.match(r'proj([\w]*).', file)
     if suffix:
-        print('File name is: ' + file)
-        print('Suffix of this file is: ' + suffix.group(1))
         dir_name = check_if_directory_exists(suffix.group(1))
-        #move_file(file, dir_name)
+    else:
+        create_misc_directory()
