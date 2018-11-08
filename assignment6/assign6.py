58002af959a23186a4c9096d90f710b83aeadc27
Rafael Rodriguez <rrodriguez1989@gmail.com>
2018-11-07 18:03:13 -0600

    Finished main assignment, started work on extra credit

--- assignment6/assign6.py
+++ assignment6/assign6.py
@@ -19,11 +19,7 @@ def create_misc_directory():
     temp_dir_name = current_directory + '/misc'
     if not os.path.isdir(temp_dir_name):
         os.mkdir(temp_dir_name)
-
-
-# Move the file to the specified directory
-def move_file(file_name, destination):
-    shutil.move(file, dest)
+    return temp_dir_name
 
 
 # Verify the correct number of arguments
@@ -31,6 +27,7 @@ if len(sys.argv) < 2:
     print('Error: Not enough arguments')
     print('Usage: [python file] [directory path]')
     sys.exit(1)
+
 
 # Check if directory argument is actually a directory
 if not os.path.isdir(sys.argv[1]):
@@ -44,10 +41,16 @@ current_directory = sys.argv[1]
 # Get all files in the directory
 all_files = os.listdir(current_directory)
 
+# Get the optional argument if it exists
+if sys.argv[2]:
+    zip_assignment = sys.argv[2]
+
 # Iterate through each file in the directory
 for file in all_files:
     suffix = re.match(r'proj([\w]*).', file)
     if suffix:
         dir_name = check_if_directory_exists(suffix.group(1))
+        shutil.move(current_directory + '/' + file, dir_name)
     else:
-        create_misc_directory()
+        misc_dir = create_misc_directory()
+        shutil.move(current_directory + '/' + file, misc_dir)
