a9442de0427e64d2d44fa10b43cf43a9ae374fb4
mat574 <mat574@fox01.cs.utsa.edu>
2018-12-09 19:54:49 -0600

    Work on low level io final exam review question

--- final_review/lowlevelio.c
+++ final_review/lowlevelio.c
@@ -3,13 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc == 2)
-        printf("Correct number of arguments supplied!\n");
-    else if(argc > 2)
-        printf("Too many arguments supplied!\n");
-    else(argc < 2)
-        printf("Not enough arguments supplied!\n");
-    
     printf("First arguement is: %s\n", argv[0]);
     printf("Second argument is: %s\n", argv[1]);
+    printf("Third argument is: %s\n", argv[2]);
 }
